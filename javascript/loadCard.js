50cfe1aa4fc90445a9f6f432346f6db3351c1cc3
HazelLiang <crocodilestear@gmail.com>
2019-01-29 10:48:13 +0800

    卡片移動(translateX, translateY)（移動不自然 捨棄）

--- javascript/loadCard.js
+++ javascript/loadCard.js
@@ -15,65 +15,91 @@ loadCard = n => {
         },timer*index);
        
     });
+    
+
     cards[(n-1)].addEventListener("transitionend", ()=>{
         cards[0].classList.add("flip");
-        cards[0].addEventListener("transitionend", () => {
+        const addFerry = () => {
             cards[0].classList.add("ferry");
-            // cards[0].addEventListener("touchend", event => {
-            //     cards[0].classList.remove("ferry");
-            // }, false);
-        });
-    cards.forEach(card => {
-        const touch = event => {
-            // event.preventDefault();
-            // console.log(card.classList.contains("ferry") )
-            // card.classList.contains("ferry") ? 
-            // card.classList.remove("ferry"): null;
-            if(event.targetTouches.length === 1){ 
+            cards[0].addEventListener("touchend", event => {
+                cards[0].classList.remove("ferry");
+            }, false);
+            cards[0].removeEventListener("transitionend", addFerry);
+        }
+        cards[0].addEventListener("transitionend", addFerry, false);
 
-                //(start)
-                let touchPoint = event.targetTouches[0];//獲取觸摸的初始位置 touchPoint.clientX & touchPoint.clientY
-                let startX = touchPoint.pageX; 
-                let startY = touchPoint.pageY;
-                let shiftX = touchPoint.clientX - card.getBoundingClientRect().left;
-                let shiftY = touchPoint.clientY - card.getBoundingClientRect().top;
+        cards.forEach(card => {
+            const touch = event => {
+                // event.preventDefault();
+                if(event.targetTouches.length === 1){ 
+                    //(start)
+                    let touchPoint = event.targetTouches[0];//獲取觸摸的位置 touchPoint.clientX & touchPoint.clientY
+                    let startT = + new Date();
+                    let isMove = false;
+                    let isTouchEnd = false;
+                    let startX = touchPoint.pageX; 
+                    let startY = touchPoint.pageY;
+                    let shiftX = touchPoint.clientX - card.getBoundingClientRect().left;
+                    let shiftY = touchPoint.clientY - card.getBoundingClientRect().top;
+                    let initialPosX = 0;
+                    let initialPosY = 0;
+                    // let currentPositionX = 0; 
+                    // let currentPositionY = 0; 
+                    let maxX = 50; 
+                    let maxY = 50; 
+                    let moveLength = 0; 
+                    // initialPosX = currentPositionX;  
+                    // initialPosY = currentPositionY;  
 
-                card.style.left = 150 + startX - shiftX + 'px';
-                card.style.top = 225 + startY - shiftY + 'px';
-                card.classList.remove("move-in");
-    
-                // centers the card at (pageX, pageY) coordinates
-                const moveAt = (pageX, pageY) => {
-                    let maxShiftX = 20;
-                    let maxShiftY = 20;
-                    // (pageX - startX) > 
-                    card.style.left = 150 + pageX - shiftX + 'px';
-                    card.style.top = 225 + pageY - shiftY + 'px';
-                }
-    
-                moveAt(touchPoint.pageX, touchPoint.pageY);
-                
-                // move the card on mousemove
-                document.ontouchmove = event => {
-                    // event.preventDefault();
-                    let touchPoint = event.targetTouches[0]
-                    console.log(card)
-                    moveAt(touchPoint.pageX, touchPoint.pageY);
+                    card.style.left =  50 + '%'; //初始位置
+                    card.style.top = 50 + '%';
+                    card.classList.remove("move-in");
+                    // card.style.transition = 0.2 + 's'; // test
                     
-                }
-            
-                // drop the card, remove unneeded handlers
-                document.ontouchend = () => {
-                    card.style.left = 150 + startX - shiftX + 'px';
-                    card.style.top = 225 + startY - shiftY + 'px';
-                    document.ontouchend = null;
-                    document.ontouchmove = null;
+                    document.ontouchmove = event => {
+                        // event.preventDefault();
+                        if (isTouchEnd) return ;
+                        let touchPoint = event.targetTouches[0];
+                        let deltaX = touchPoint.pageX - startX;
+                        let deltaY = touchPoint.pageY - startY;
+                        let translateX = initialPosX + deltaX;
+                        let translateY = initialPosY + deltaY;
+
+                        if(translateX > maxX){
+                            translateX = maxX;
+                        }
+                        if(translateY > maxY){
+                            translateY = maxY;
+                        }
+                        if(translateX < -maxX){
+                            translateX = -maxX;
+                        }
+                        if(translateY < -maxY){
+                            translateY = -maxY;
+                        }
+                        console.log(`translate3d(${translateX}px, ${translateY}px, 0)`);
+                        card.style.transform = `translate3d(${translateX}px, ${translateY}px, 0)`;
+                        // card.style.left = 150 + touchPoint.pageX - shiftX + 'px';
+                        // card.style.top = 225 + touchPoint.pageY - shiftY + 'px';
+                        
+                    }
+                
+                    // drop the card, remove unneeded handlers
+                    document.ontouchend = () => {
+                        card.style.left =  50 + '%';//回到初始位置
+                        card.style.top = 50 + '%';
+                        document.ontouchend = null;
+                        document.ontouchmove = null;
+                    }
                 }
             }
-        }
-        card.addEventListener("touchstart", touch, false);
-    });
-});
+            if(card.classList.contains("flip")){
+                card.addEventListener("transitionend", () => {
+                    card.addEventListener("touchstart", touch, false);
+                },false);
+            } 
+        });
+    }, false);
 
 }
    
